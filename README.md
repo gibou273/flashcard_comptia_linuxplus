d48a51ac30baaca626c82dea0b64aedc1a30c70a
gibou273 <101921033+gibou273@users.noreply.github.com>
2024-03-26 01:43:55 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+What is this App About?
+
+This flash card app contains all the acronyms you will find in the CompTIA Linux+ exams, and using this flashcard
+
+will help you easily memorise all the acronyms needed for the exams.
---
+++ convert_to_csv.py
@@ -0,0 +1,5 @@
+with open("data/acronyms.txt", "r") as file:
+    content = file.readlines()
+    for line in content:
+        with open("data/acronyms.csv", "a") as myFile:
+            myFile.write(",".join(line.split(" ", 1)))
